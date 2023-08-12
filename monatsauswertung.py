98ac0489d6765a013b7f8363d8a4691b7277203b
EisenFred <001Luc.eisenbach@gmail.com>
2023-08-13 00:39:48 +0200

    added settings.py

--- monatsauswertung.py
+++ monatsauswertung.py
@@ -3,13 +3,10 @@ from openpyxl.styles import PatternFill
 from load_csv import load_csv
 import datetime
 import openpyxl
+import settings as s
 
 # Spalte vor das Datum mit dem Wochentag
 
-EXCEL_FILENAME = 'output.xlsx'
-CSV_FILENAME = "SB56_t30_Ferien.csv"
-MONTH = 7
-YEAR = 2023
 
 def generate_dates(year, month):
     num_days = (datetime.date(year, month + 1, 1) - datetime.date(year, month, 1)).days
@@ -17,14 +14,14 @@ def generate_dates(year, month):
     return dates
 
 def monatsauswertung():
-    data = load_csv(CSV_FILENAME)
+    data = load_csv(s.CSV_FILENAME)
     columns = ['Linie', 'Soll', 'Ist', 'Verspaetung', 'Datum', 'Wochentag']
     df_csv = pd.DataFrame(data, columns=columns)
     df_csv['Soll'] = pd.to_datetime(df_csv['Soll'], format='%H:%M')
     df_csv['Ist'] = pd.to_datetime(df_csv['Ist'], format='%H:%M')
     df_csv['Datum'] = pd.to_datetime(df_csv['Datum'], format='%d.%m.%Y', dayfirst=True)
-    start_date = pd.to_datetime(f'2023-{MONTH:02d}-01')
-    end_date = pd.to_datetime(f'2023-{MONTH:02d}-31')
+    start_date = pd.to_datetime(f'2023-{s.MONTH:02d}-01')
+    end_date = pd.to_datetime(f'2023-{s.MONTH:02d}-31')
     df_csv = df_csv[(df_csv['Datum'] >= start_date) & (df_csv['Datum'] <= end_date)]
 
     rows = 32
@@ -32,7 +29,7 @@ def monatsauswertung():
     df_auswertung = pd.DataFrame(index=range(rows), columns=columns2)
 
     for index_day_in_month in range(1,32):
-        date_day = pd.to_datetime(f'2023-{MONTH:02d}-{index_day_in_month:02d}')
+        date_day = pd.to_datetime(f'2023-{s.MONTH:02d}-{index_day_in_month:02d}')
         df_thisDay = df_csv[(df_csv['Datum'] == date_day)]
         df_thisDay = df_thisDay.copy() # Verhindert eine Warnung die durch die beiden folgenden Zeilen sonst entstehen würden.
         df_thisDay['Ist'] = df_thisDay['Ist'].dt.strftime('%H:%M')
@@ -47,13 +44,13 @@ def monatsauswertung():
                     if df_auswertung.columns[time_day] == df_thisDay.loc[index_df_thisDay,'Soll']:
                         df_auswertung.iloc[index_day_in_month-1,time_day] = df_thisDay.loc[index_df_thisDay, 'Verspaetung']
                         index_df_thisDay += 1
-    array = generate_dates(YEAR, MONTH)
+    array = generate_dates(s.YEAR, s.MONTH)
     new_column = pd.Series(array, name='Datum')
     df_auswertung.insert(0, 'Datum', new_column)
 
-    df_auswertung.to_excel(EXCEL_FILENAME, index=False)
+    df_auswertung.to_excel(s.EXCEL_FILENAME, index=False)
 
-    workbook = openpyxl.load_workbook(EXCEL_FILENAME)
+    workbook = openpyxl.load_workbook(s.EXCEL_FILENAME)
     worksheet = workbook['Sheet1']
     worksheet.column_dimensions[openpyxl.utils.get_column_letter(1)].width = 11
     for index in range(2,34):
@@ -64,20 +61,20 @@ def monatsauswertung():
     for row in worksheet[data_range]:
         for cell in row:
             if cell.value == 0:
-                cell.fill = PatternFill(start_color='9bbb59', end_color='9bbb59', fill_type='solid')
+                cell.fill = PatternFill(start_color=s.COLOR0, end_color=s.COLOR0, fill_type='solid')
             if cell.value == 1:
-                cell.fill = PatternFill(start_color='b6be3c', end_color='b6be3c', fill_type='solid')
+                cell.fill = PatternFill(start_color=s.COLOR1, end_color=s.COLOR1, fill_type='solid')
             if cell.value == 2:
-                cell.fill = PatternFill(start_color='d2c21e', end_color='d2c21e', fill_type='solid')
+                cell.fill = PatternFill(start_color=s.COLOR2, end_color=s.COLOR2, fill_type='solid')
             if cell.value == 3:
-                cell.fill = PatternFill(start_color='eec600', end_color='eec600', fill_type='solid')
+                cell.fill = PatternFill(start_color=s.COLOR3, end_color=s.COLOR3, fill_type='solid')
             if cell.value == 4:
-                cell.fill = PatternFill(start_color='d78b26', end_color='d78b26', fill_type='solid')
+                cell.fill = PatternFill(start_color=s.COLOR4, end_color=s.COLOR4, fill_type='solid')
             if cell.value is not None and cell.value >= 5:
-                cell.fill = PatternFill(start_color='c0504d', end_color='c0504d', fill_type='solid')
+                cell.fill = PatternFill(start_color=s.COLOR5, end_color=s.COLOR5, fill_type='solid')
             if cell.value is not None and cell.value >= 10:
-                cell.fill = PatternFill(start_color='8e1e1b', end_color='8e1e1b', fill_type='solid')
-    workbook.save(EXCEL_FILENAME)
+                cell.fill = PatternFill(start_color=s.COLOR6, end_color=s.COLOR6, fill_type='solid')
+    workbook.save(s.EXCEL_FILENAME)
 
 monatsauswertung()
 
---
+++ settings.py
@@ -0,0 +1,22 @@
+# Dateiname der Excel-Datei, in der die Auswertung gespeichert wird. (Datei wird automatisch erstellt)
+EXCEL_FILENAME = 'output.xlsx'
+# Dateiname der CSV-Datei mit den Rohdaten.
+CSV_FILENAME = "SB56_t30_Ferien.csv"
+# Monat und Jahr das ausgewertet werden soll.
+MONTH = 7
+YEAR = 2023
+# Farben für die verschiedenen Verspätungen.
+# Verspätung = 0 Minute
+COLOR0 = '9bbb59'
+# Verspätung = 1 Minute
+COLOR1 = 'b6be3c'
+# Verspätung = 2 Minute
+COLOR2 = 'd2c21e'
+# Verspätung = 3 Minute
+COLOR3 = 'eec600'
+# Verspätung = 4 Minute
+COLOR4 = 'd78b26'
+# Verspätung >= 5 Minute
+COLOR5 = 'c0504d'
+# Verspätung >= 10 Minute
+COLOR6 = '8e1e1b'
